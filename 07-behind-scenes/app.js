64702778960233fd25c3f84b166afd9b19d31a80
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-04-27 08:49:00 +0200

    vue behind scene

---
+++ 07-behind-scenes/app.js
@@ -0,0 +1,46 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: "",
+      message: "Vue is great!",
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+  beforeCreate() {
+    console.log("beforeCreate()");
+  },
+  created() {
+    console.log("created()");
+  },
+  beforeMount() {
+    console.log("beforeMounted()");
+  },
+  mounted() {
+    console.log("Mounted()");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate()");
+  },
+  updated() {
+    console.log("updated()");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount()")
+  },
+  unmounted(){
+    console.log("unmouned()");
+  }
+});
+
+app.mount("#app");
+
+setTimeout(function () {
+  app.unmount();
+}, 3000 )
