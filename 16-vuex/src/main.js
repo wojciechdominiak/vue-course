23c1ec7ad7e5ffdba105f4ae69db904d756b38c4
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-05-07 13:25:46 +0200

    vuex more

--- 16-vuex/src/main.js
+++ 16-vuex/src/main.js
@@ -1,77 +1,7 @@
 import { createApp } from 'vue';
-import { createStore } from 'vuex';
 
 import App from './App.vue';
-
-const counterModule = {
-  namespaced: true,
-  state() {
-    return { counter: 0 };
-  },
-  mutations: {
-    increament(state) {
-      state.counter++;
-    },
-    increase(state, val) {
-      console.log(state);
-      state.counter = state.counter + val.value;
-    },
-  },
-  actions: {
-    increament(context) {
-      setTimeout(function () {
-        context.commit('increament');
-      }, 2000);
-    },
-    increase(context, val) {
-      setTimeout(function () {
-        context.commit('increase', val);
-      }, 2000);
-      console.log(context);
-    },
-  },
-  getters: {
-    finalCounter(state) {
-      return state.counter * 3;
-    },
-    normalizeCounter(_state, getters) {
-      const finalCounter = getters.finalCounter;
-      if (finalCounter < 0) {
-        return 0;
-      } else if (finalCounter > 100) {
-        return 100;
-      } else {
-        return finalCounter;
-      }
-    },
-  },
-};
-
-const store = createStore({
-  modules: {
-    counter: counterModule,
-  },
-  state() {
-    return {
-      isLoged: false,
-    };
-  },
-  mutations: {
-    logToggle(state) {
-      state.isLoged = !state.isLoged;
-    },
-  },
-  actions: {
-    logToggle(context) {
-      context.commit('logToggle');
-    },
-  },
-  getters: {
-    isLoged(state) {
-      return state.isLoged;
-    },
-  },
-});
+import store from './store/index.js';
 
 const app = createApp(App);
 
