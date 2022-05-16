976fdb915ec79e8e336d7add26cb819732dcb6d4
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-05-16 16:41:35 +0200

    composition API deep

--- 17-compositionApi/src/App.vue
+++ 17-compositionApi/src/App.vue
@@ -1,25 +1,40 @@
 <template>
   <section class="container">
-    <h1>{{title}}</h1>
-    <h2>{{ user.name }}</h2>
+    <h1>{{ title }}</h1>
+    <h2>{{ name }}</h2>
     <h3>{{ user.age }}</h3>
     <button @click="change">Change</button>
+    <div>
+      <input type="text" placeholder="First name" v-model="firstName" />
+      <input type="text" placeholder="Last name" v-model="lastName" />
+    </div>
   </section>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, reactive, computed, watch } from 'vue';
 
 const title = ref('Composition API');
+const firstName = ref('');
+const lastName = ref('');
 
 const user = reactive({
-  name: 'Wojciech',
   age: 31,
 });
 
-function change () {
-  user.age=user.age+2;
-  title.value='Great';
+
+watch([firstName, lastName], function(newValues, oldValues) {
+  console.log('Old ' + oldValues);
+  console.log('New ' + newValues);
+});
+
+const name = computed(() => {
+  return firstName.value + ' ' + lastName.value;
+});
+
+function change() {
+  user.age = user.age + 2;
+  title.value = 'Great';
 }
 </script>
 
