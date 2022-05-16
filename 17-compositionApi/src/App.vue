5437951d20925cb46d64a8b3b279e6ab50038d6f
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-05-16 14:04:33 +0200

    introduction to composition API

---
+++ 17-compositionApi/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="container">
+    <h1>{{title}}</h1>
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
+    <button @click="change">Change</button>
+  </section>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue';
+
+const title = ref('Composition API');
+
+const user = reactive({
+  name: 'Wojciech',
+  age: 31,
+});
+
+function change () {
+  user.age=user.age+2;
+  title.value='Great';
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
