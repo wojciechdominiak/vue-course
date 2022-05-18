1fd284657d9e36d8deff722269705f8762958542
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-05-18 09:54:03 +0200

    migration to compositionAPI app

---
+++ 18-compositionAPI-2/src/components/UI/BaseSearch.vue
@@ -0,0 +1,53 @@
+<template>
+  <form>
+    <input
+      type="search"
+      @input="search"
+      :value="searchTerm"
+      placeholder="Filter items"
+    />
+  </form>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+
+/* eslint-disable no-unused-vars */
+const emit = defineEmits(['search']);
+
+const props = defineProps({
+  searchTerm: String,
+});
+
+function search(event) {
+  emit('search', event.target.value);
+}
+</script>
+<!-- <script>
+export default {
+  props: ['searchTerm'],
+  emits: ['search'],
+  methods: {
+    search(event) {
+      this.$emit('search', event.target.value);
+    },
+  },
+};
+</script> -->
+
+<style scoped>
+input {
+  font: inherit;
+  width: 100%;
+  display: block;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus {
+  outline: none;
+  border-color: #00006b;
+  background-color: #eeeeff;
+}
+</style>
---
+++ 18-compositionAPI-2/src/components/projects/ProjectItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h3>{{ title }}</h3>
+  </li>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+/* eslint-disable no-unused-vars */
+const props = defineProps({
+  title: String,
+});
+</script>
+
+<!-- <script>
+export default {
+  props: ['title'],
+};
+</script>
+ -->
+<style scoped>
+li {
+  background-color: #00006b;
+  color: white;
+  padding: 0.5rem;
+  margin: 1rem 0;
+}
+
+li h3 {
+  margin: 0;
+}
+</style>
