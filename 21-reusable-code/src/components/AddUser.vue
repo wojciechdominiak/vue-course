08aa39f3e4d5cc1e28bba32be33ad1728accdb10
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-05-20 12:23:07 +0200

    mixins

---
+++ 21-reusable-code/src/components/AddUser.vue
@@ -0,0 +1,23 @@
+<template>
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
+    <p>Do you want to continue with adding a user?</p>
+  </user-alert>
+  <section>
+    <h2>Add a User</h2>
+    <button @click="showAlert">Add User</button>
+  </section>
+</template>
+
+<script>
+import alertMixin from '../mixins/alert.js';
+
+export default {
+  data() {
+    return {
+      alertIsVisible: true,
+      alertTitle: 'Delete User?',
+    };
+  },
+  mixins: [alertMixin],
+};
+</script>
